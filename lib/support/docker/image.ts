96b9686af2671a1faf1b8a13e3ad9b4213ef5704
Diego Hernandes <iamhernandev@gmail.com>
2019-02-14 16:08:15 -0200

    image pull helper

---
+++ lib/support/docker/image.ts
@@ -0,0 +1,14 @@
+import { docker } from "./docker";
+import { connectStdin, connectStdout } from "./run";
+import { detectDetachingKeys } from "./tty";
+
+export const pullImage = (imageName) => docker.pull(imageName, {})
+  .then((stream) => {
+    // connect stdout.
+    connectStdout(stream);
+    // connect stdin.
+    connectStdin(stream);
+    // detect keys to detach process.
+    detectDetachingKeys(stream);
+  })
+  .catch(console.log);
