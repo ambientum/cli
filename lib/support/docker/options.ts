bb4520c79988bfbfc2ba396db33cf9af689f5d15
Diego Hernandes <iamhernandev@gmail.com>
2019-02-14 16:07:53 -0200

    default docker options

---
+++ lib/support/docker/options.ts
@@ -0,0 +1,33 @@
+/**
+ * Default Docker run options.
+ *
+ * @type {{Object}}
+ */
+export const defaultOptions = {
+  Hostname: "",
+  // 'User': '',
+  AttachStdin: true,
+  AttachStdout: true,
+  AttachStderr: true,
+  Tty: true,
+  OpenStdin: true,
+  StdinOnce: false,
+  Env: null,
+  // 'Cmd': ['sh'],
+  Dns: ["8.8.8.8", "8.8.4.4"],
+  Image: "alpine:3.8",
+  Volumes: {},
+  VolumesFrom: [],
+};
+
+/**
+ * Default attach options (TTY options).
+ *
+ * @type {{stdin: boolean, stdout: boolean, stream: boolean, stderr: boolean}}
+ */
+export const defaultAttachOptions = {
+  stream: true,
+  stdin: true,
+  stdout: true,
+  stderr: true,
+};
