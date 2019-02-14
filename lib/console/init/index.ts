d8ef1f60f32b60959c70d3ede8f93342748cb6c2
Diego Hernandes <iamhernandev@gmail.com>
2019-02-14 17:34:49 -0200

    readme initial

---
+++ lib/console/init/index.ts
@@ -0,0 +1,14 @@
+// import CommandGroup class.
+import { CommandGroup } from "lib/support/console";
+// import commands classes.
+import { ConfigCommand } from "./commands/config";
+import { NodeRunnerCommand } from "./commands/node-runner";
+import { PhpRunnerCommand } from "./commands/php-runner";
+
+// factory config group.
+export default () => (new CommandGroup([
+  new PhpRunnerCommand(),
+  new NodeRunnerCommand(),
+  new ConfigCommand(),
+]));
+
